d6b3318dc046b8e36d949d8be437da792ac04cbb
rhoyyyalvinnn <94331000+rhoyyyalvinnn@users.noreply.github.com>
2025-05-05 21:32:22 +0800

    some dashboard changes + app name change to XenoDent + footer

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link>
+                        {{ __('About XenoDent') }}
+                    </x-nav-link>
+                    <x-nav-link>
+                        {{ __('How to Use') }}
+                    </x-nav-link>
 
 
                     
